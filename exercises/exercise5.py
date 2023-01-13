f9ceb329dbc97fa8da313a4dd56d6f88bbf5b6f6
Mike <miguel.amsuv@outlook.com>
2023-01-13 11:23:50 -0600

    Complete exercise5.py by using while loop

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -5,13 +5,18 @@ Cover the use of loops for simple problems
 #Repeatedly read numbers until user enters done, add all numbers entered print total, count, and the average at the end. Detect mistakes if not a number
 
 tot = 0 
+count = 0
 while True:
     number = input('Enter a number: ')
-    if number = 'done':
+    if number == 'done':
         break
-    else:
-        try:
-            float(number)
-            tot = tot + number
-        except:
-            print('bad data')
+    try:
+        number = float(number)   
+        tot = tot + number
+        count += 1
+    except:
+        print('Invalid input')
+if count != 0:
+    print(tot,count,tot/count)
+else: 
+    print('No result')
