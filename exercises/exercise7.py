360b0ff3c6ec5f579e65e7bba78e83aa4e9b6cfb
Mike <miguel.amsuv@outlook.com>
2023-01-21 15:01:07 -0600

    Create baseline for exercise7.py

---
+++ exercises/exercise7.py
@@ -0,0 +1,5 @@
+"""
+Deal with string transformations and for loops while opening external files
+"""
+#Open the file from the directory home
+txtfile = open("./exercises/exercise_files/mbox-short.txt")
