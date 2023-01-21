8227c3a028068bbbe18461d5cbe75f5b3c8828d6
Mike <miguel.amsuv@outlook.com>
2023-01-21 15:05:51 -0600

    Complete exercise7.py by printing line by line

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -1,5 +1,10 @@
 """
 Deal with string transformations and for loops while opening external files
 """
-#Open the file from the directory home
-txtfile = open("./exercises/exercise_files/mbox-short.txt")
+#Open the file from the directory exercise folder where the program is located
+txtfile = open("./exercise_files/mbox-short.txt")
+
+#Write program to print lines in the file line by line in all caps
+for line in txtfile :
+    clean_line = line.rstrip()
+    print(clean_line.upper())
