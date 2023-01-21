7afc12ad9887c92ca737755de45593841d733857
Mike <miguel.amsuv@outlook.com>
2023-01-21 13:52:57 -0600

    Complete exercise6.py on string functions.

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -4,6 +4,7 @@ Cover additional string functions and transformations through an exercise
 
 #take the string and extract the number at the end then use float to convert it.
 
-string = 'X-DSPAM-Confidence: 0.8475'
-str_start = string.find(' ')
-print(str_start)
+string = 'X-DSPAM-Confidence: 0.8475 '
+str_start = int(string.find(' '))
+str_num = float(string[str_start+1:])
+print(str_num)
